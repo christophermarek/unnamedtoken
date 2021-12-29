493daa6380da4d54b62229f3af5bd57fe7f5e139
christophermarek <christophermarek42@gmail.com>
2021-12-29 12:16:54 -0500

    mobile setup almost done

--- website_code/components/tokenomicsBox.tsx
+++ website_code/components/tokenomicsBox.tsx
@@ -11,6 +11,13 @@ export const TokenomicsBox: React.FC<TokenomicsProps> = ({ tokenName, tokenTicke
 
     const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
 
+    const tokenomicsInfo = [
+        'Total supply: 1.000.000.000.000.000',
+        'There is a 15% buy tax.An extra 1% fee is applied to all sales to encourage holding, discourage swing trading and to lessen whale manipulation.At times, our tokenomics may change in response to market conditions.',
+        'The token contract employs a static rewards system.The 15% buy tax is split as follows: 11% is redistributed to holders 2% is used to fuel the liquidity pool 2% is allocated to the Marketing wallet',
+        'Token supply: 50% burned, what if we keep a burn wallet and schedule a burn every week until depleted? 2% team 20% liquidity pool 25% presale 3% marketing'
+    ]
+
     return (
         <div className='TokenomicsPage' id='tokenomics'>
             <div className={'headerBar marginTop1 marginBottom1'}>
@@ -26,44 +33,27 @@ export const TokenomicsBox: React.FC<TokenomicsProps> = ({ tokenName, tokenTicke
                     animate={{
                         opacity: 1,
                     }}>
-                    <div>
                         {!isPortrait &&
                             <>
                                 <p>{tokenName} Tokenomics</p>
                                 <p>{tokenTicker}</p>
                             </>
                         }
-
-                        <p>Total supply: 1.000.000.000.000.000</p>
-                        <p>There is a 15% buy tax.An extra 1% fee is applied to all sales to
-                            encourage holding, discourage swing trading and to lessen whale
-                            manipulation.At times, our tokenomics may change in response to
-                            market conditions.</p>
-
-                        <p>The token contract employs a static rewards system.The 15% buy
-                            tax is split as follows:
-                            11% is redistributed to holders
-                            2% is used to fuel the liquidity pool
-                            2% is allocated to the Marketing wallet
-                        </p>
-                        <p>
-                            Token supply:
-                            50% burned, what if we keep a burn wallet and schedule a burn every week until depleted?
-                            2% team
-                            20% liquidity pool
-                            25% presale
-                            3% marketing
-                        </p>
-                    </div>
+                        <>
+                            <p className={'tokenomicsText'}>{tokenomicsInfo[0]}</p>
+                            <p className={'tokenomicsText'}>{tokenomicsInfo[1]}</p>
+                            <p className={'tokenomicsText'}>{tokenomicsInfo[2]}</p>
+                            <p className={'tokenomicsText'}>{tokenomicsInfo[3]}</p>
+                        </>
 
                     <motion.button className='purplebutton font24 font' whileHover={{ scale: 1.1, color: 'black' }}
                         whileTap={{ scale: 0.9 }}>
                         Buy Now
                     </motion.button>
                 </motion.div>
-            </div>
+            </div >
 
-        </div>
+        </div >
     )
 
 }
